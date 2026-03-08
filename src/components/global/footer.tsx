52ec1475e1eeec06b52ce1798de43e09efa9ebe7
lorem <lorem.ipsum.crypto@gmail.com>
2026-03-08 19:44:29 -0400

    fix: footer

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -22,7 +22,9 @@ export const Footer = () => {
     <footer className="mt-auto border-t">
       <div className="w-full lg:w-7xl flex flex-row justify-between mx-auto my-4">
         <Link href="/" className="hover:cursor-pointer">
-          <div className="mx-4 mt-1">Yild Finance</div>
+          <div className="mx-4 mt-1 font-black tracking-wider">
+            Yild Finance
+          </div>
         </Link>
         <div className="flex flex-row gap-2 mx-4 items-center">
           <Fuel size={18} />
